e92d21e75340f12ab023ac8e2647d5ecb875ae1d
Vika-Koval <149317864+Vika-Koval@users.noreply.github.com>
2024-10-02 23:01:31 +0300

    Update Demo.java

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -4,10 +4,8 @@ import lotr.Character;
 import lotr.CharacterFactory;
 public class Demo {
     public static void main(String args[]) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-
         Character character1 = CharacterFactory.createCharacter();
         Character character2 = CharacterFactory.createCharacter();
-
         GameManager.fight(character1, character2);
     }
 }
