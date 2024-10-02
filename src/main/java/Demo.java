ada387432a55efba7be6b262c1159151210cddb6
Vika-Koval <Koval V>
2024-10-02 22:52:40 +0300

    add my lab

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,13 @@
+import java.lang.reflect.InvocationTargetException;
+
+import lotr.Character;
+import lotr.CharacterFactory;
 public class Demo {
-    public static void main(String args[]) {
-        System.out.println("Hello, world!");
+    public static void main(String args[]) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+
+        Character character1 = CharacterFactory.createCharacter();
+        Character character2 = CharacterFactory.createCharacter();
+
+        GameManager.fight(character1, character2);
     }
 }
